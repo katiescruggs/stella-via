2d6f420d2670e74778bde843d2c867fa17dca676
julie hawkins <julabi@gmail.com>
2017-12-20 21:06:04 -0700

    builds fetch function in APOD

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -5,16 +5,38 @@ class APOD extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      image: {uri: 'https://www.rugbywarfare.com/store/wp-content/uploads/2017/04/random-image-005.jpg'}
     };
   }
 
+  fetchAPOD = () => {
+    console.log('function called')
+    // try {
+      fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`)
+      .then(data => JSON.parse(data))
+      .then(data => console.log(data))
+      // console.log(apodData)
+      // const image = {uri: apodData}
+      // this.setState({ image })
+    // } catch(error) {
+    //   const image = {uri: 'https://www.rugbywarfare.com/store/wp-content/uploads/2017/04/random-image-005.jpg'}
+    //   this.setState({ image })
+    // }
+    
+  }
+
+  componentDidMount() {
+    console.log('did mount')
+    this.fetchAPOD()
+  }
+
   render() {
+    console.log('hi')
     return (
       <View>
         <Image
           style={{width: 50, height: 50}} 
-          source={{uri: 'http://globalmedicalco.com/photos/globalmedicalco/20/100048.jpg'}}
+          source={this.state.image}
         />
       </View>
     )
