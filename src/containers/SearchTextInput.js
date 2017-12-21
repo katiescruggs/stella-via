da24b80ce3ae486a4b464d216de0125fba3951ed
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-20 21:00:08 -0700

    delete boiler plate from App and create SearchTextInput component

--- src/containers/SearchTextInput.js
+++ src/containers/SearchTextInput.js
@@ -1,22 +1,89 @@
 import React, { Component } from 'react';
-import { AppRegistry, TextInput, View, Text } from 'react-native';
+import { StyleSheet, AppRegistry, TextInput, View, Text } from 'react-native';
+import getLocation from '../helpers/getLocation.js';
+import getDate from '../helpers/getDate.js';
 
 class SearchTextInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'Search'
+      text: ''
     };
   }
 
+  async componentDidMount() {
+    const now = getDate();
+    this.setState({now});
+
+    await this.getData();
+  }
+
+  async getData() {
+    const location = await getLocation();
+    console.log(location);
+    this.setState({lat: location.lat, lon: location.lon});
+  }
+
   render() {
+    let day, month, date, year;
+    let lat, lon;
+
+    let text = this.state.text;
+
+    if(this.state.lat && this.state.lon) {
+      lat = this.state.lat;
+      lon = this.state.lon;
+    }
+
+    if(this.state.now) {
+      day = this.state.now.day;
+      month = this.state.now.month;
+      date = this.state.now.date;
+      year = this.state.now.year;
+    }
+
     return (
-      <TextInput 
-        style={{height: 40, width: 80, backgroundColor: 'magenta'}}
-        value={this.state.value}
+      <View style={styles.container}>
+        <Text style={styles.h1}>Stella Via</Text>
+        <Text style={styles.h2}>Your Night Sky</Text>
+        <Text style={styles.p}>{`${day}, ${month} ${date}, ${year}`}</Text>
+        <Text style={styles.p}>{`at latitude: ${lat}, longitude: ${lon}`}</Text>
+        <TextInput 
+          style={styles.input}
+          value={text}
+          placeholder='Search for a different location.'
+          onChangeText={(text) => this.setState({text})}
         />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#9999FF',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 420
+  },
+
+  h1: {
+    fontSize: 40
+  },
+  h2: {
+    fontSize: 24
+  },
+  input: {
+    fontSize: 16,
+    height: 40,
+    width: 300,
+    borderColor: '#FFCC66',
+    borderWidth: 2
+  },
+  p: {
+    fontSize: 16
+  }
+});
 
 export default SearchTextInput;
