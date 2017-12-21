3481e670cee452f088629eb56b3fac348a76dd09
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-20 19:18:01 -0700

    create file structure and test importing component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchTextInput from './src/containers/SearchTextInput.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <SearchTextInput />
         <Text h1>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake hello your phone to open the developer menu.</Text>
