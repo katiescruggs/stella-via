0d7886d52fee0d9b0b7e51f5c8f139d5de7778e1
julie hawkins <julabi@gmail.com>
2017-12-26 17:10:14 -0700

    adds constellation folder and file

---
+++ constellations/constellations.js
@@ -0,0 +1,113 @@
+[
+  {
+    name: 'Leo',
+    translation: 'The Lion',
+    coords: {
+      ra: '11 hours',
+      dec: '15 degrees',
+      visible: 'between latitudes 90 and -65 degrees',
+      bestSeen: 'April',
+    },
+    stars: [
+      'Regulus', 
+      'Denobola', 
+      'Algieba', 
+      'Zosma', 
+      'Ras Elased Australis', 
+      'Adhafera', 
+      'Chort', 
+      'Al Minliar al Asad', 
+      'Alterf', 
+      'Ras Elased Borealis', 
+      'Subra'
+    ],
+    description: 'One of the 12 constellations. Leo is the Nemean Lion which was killed by Hercules on one of his great quests. Legend says that the lion had a hide that could not punctured by iron, bronze or stone. Since he couldn\'t reason with the ferocious beast, Hercules strangled it to death and the local people were very grateful. The Chaldeans associated Leo with the sun since it is in the sky during the summer solstice (although this is no longer true, due to the precession of the Earth\'s axis). Since Nile floods around this time, the ancient Egyptians worshipped the celestial lion. You can find Leo in the sky by looking for the "sickle" starting at Regulus (Alpha Leonis) and following the backwards question mark. To find Regulus, use the Big Dipper as described on the Ursa Major Page. In more modern times (late 1600\'s that is!), Johannes Hevelius added the constellation Leo Minor.'
+  },
+  {
+    name: 'Virgo,
+    translation: 'The Maiden',
+    coords: {
+      ra: '13 hours',
+      dec: '0 degrees',
+      visible: 'between latitudes 80 and -80 degrees',
+      bestSeen: 'May',
+    },
+    stars: [
+      'Spica', 
+      'Zavijah', 
+      'Porrima', 
+      'Auva', 
+      'Vindemiatrix',
+      'Heze', 
+      'Zaniah', 
+      'Syrma',
+      'Rijl al Awwa'
+    ],
+    description: 'One of the 12 constellations.'
+  },
+  {
+    name: 'Libra',
+    translation: 'The Beam Balance',
+    coords: {
+      ra: '15 hours',
+      dec: '-15 degrees',
+      visible: 'between latitudes 65 and -90 degrees',
+      bestSeen: 'June',
+    },
+    stars: [
+      'Zuben Elgenubi', 
+      'Zuben Elchemali',
+      'Zuben Elakrab', 
+      'Zuben Elakribi', 
+      'Brachium'
+    ],
+    description: 'One of the 12 constellations'
+  },
+  {
+    name: 'Scorpio',
+    translation: 'The Scorpion',
+    coords: {
+      ra: '17 hours',
+      dec: '-40 degrees',
+      visible: 'between latitudes 40 and -90 degrees',
+      bestSeen: 'July',
+    },
+    stars: [
+      'Antares', 
+      'Graffias', 
+      'Dschubba', 
+      'Sargas', 
+      'Shaula', 
+      'Jabbah', 
+      'Grafias', 
+      'Alniyat', 
+      'Alniyat', 
+      'Lesath'
+    ],
+    description: 'One of the 12 constellations. Scorpius is the beast which finally killed the Great Hunter, Orion.'
+  },
+  {
+    name:,
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name:,
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  }
+]
