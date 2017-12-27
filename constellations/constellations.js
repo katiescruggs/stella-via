78c1c2bd04c177d04ec2679c7bfd5b7bf618f092
julie hawkins <julabi@gmail.com>
2017-12-26 18:44:42 -0700

    adds all constellation names

--- constellations/constellations.js
+++ constellations/constellations.js
@@ -1,5 +1,511 @@
 module.exports = [
   {
+    name: 'Andromeda',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Antlia',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Apus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Aquarius',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Aquila',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Ara',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Aries',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Auriga',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Boötes',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Caelum',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Camelopardalis',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Cancer',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Canes Venatici',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Canis Major',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Canis Minor',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Capricornus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Carina',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Cassiopeia',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Centaurus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Cepheus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Cetus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Chamaeleon',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Circinus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Columba',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },{
+    name: 'Coma Berenices',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Corona Austrina',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Corona Borealis',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Corvus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Crater',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Crux',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Cygnus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Delphinus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Dorado',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Draco',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Equuleus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Fornax',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Gemini',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Grus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Hercules',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Horologium',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Hydra',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Indus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Lacerta',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
     name: 'Leo',
     translation: 'The Lion',
     coords: {
@@ -24,26 +530,28 @@ module.exports = [
     description: 'One of the 12 constellations. Leo is the Nemean Lion which was killed by Hercules on one of his great quests. Legend says that the lion had a hide that could not punctured by iron, bronze or stone. Since he couldn\'t reason with the ferocious beast, Hercules strangled it to death and the local people were very grateful. The Chaldeans associated Leo with the sun since it is in the sky during the summer solstice (although this is no longer true, due to the precession of the Earth\'s axis). Since Nile floods around this time, the ancient Egyptians worshipped the celestial lion. You can find Leo in the sky by looking for the "sickle" starting at Regulus (Alpha Leonis) and following the backwards question mark. To find Regulus, use the Big Dipper as described on the Ursa Major Page. In more modern times (late 1600\'s that is!), Johannes Hevelius added the constellation Leo Minor.'
   },
   {
-    name: 'Virgo,
-    translation: 'The Maiden',
+    name: 'Leo Minor',
+    translation:,
     coords: {
-      ra: '13 hours',
-      dec: '0 degrees',
-      visible: 'between latitudes 80 and -80 degrees',
-      bestSeen: 'May',
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
     },
-    stars: [
-      'Spica', 
-      'Zavijah', 
-      'Porrima', 
-      'Auva', 
-      'Vindemiatrix',
-      'Heze', 
-      'Zaniah', 
-      'Syrma',
-      'Rijl al Awwa'
-    ],
-    description: 'One of the 12 constellations.'
+    stars: [],
+    description:
+  },
+  {
+    name: 'Lepus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
   },
   {
     name: 'Libra',
@@ -64,7 +572,283 @@ module.exports = [
     description: 'One of the 12 constellations'
   },
   {
-    name: 'Scorpio',
+    name: 'Lupus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Lynx',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Lyra',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Mensa',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Microscopium',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Monoceros',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Musca',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Norma',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Octans',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Ophiuchus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Orion',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Pavo',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Pegasus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Perseus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Phoenix',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Pictor',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Pisces',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Piscis Austrinus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Puppis',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Pyxis',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Reticulum',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Sagitta',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Sagittarius',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Scorpius',
     translation: 'The Scorpion',
     coords: {
       ra: '17 hours',
@@ -87,7 +871,7 @@ module.exports = [
     description: 'One of the 12 constellations. Scorpius is the beast which finally killed the Great Hunter, Orion.'
   },
   {
-    name:,
+    name: 'Sculptor',
     translation:,
     coords: {
       ra:,
@@ -99,7 +883,173 @@ module.exports = [
     description:
   },
   {
-    name:,
+    name: 'Scutum',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Serpens',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Sextans',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Taurus',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Telescopium',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Triangulum',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Triangulum Australe',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Tucana',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Ursa Major',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Ursa Minor',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Vela',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Virgo,
+    translation: 'The Maiden',
+    coords: {
+      ra: '13 hours',
+      dec: '0 degrees',
+      visible: 'between latitudes 80 and -80 degrees',
+      bestSeen: 'May',
+    },
+    stars: [
+      'Spica', 
+      'Zavijah', 
+      'Porrima', 
+      'Auva', 
+      'Vindemiatrix',
+      'Heze', 
+      'Zaniah', 
+      'Syrma',
+      'Rijl al Awwa'
+    ],
+    description: 'One of the 12 constellations.'
+  },
+  {
+    name: 'Volans',
+    translation:,
+    coords: {
+      ra:,
+      dec:,
+      visible:,
+      bestSeen:,
+    },
+    stars: [],
+    description:
+  },
+  {
+    name: 'Vulpecula',
     translation:,
     coords: {
       ra:,
