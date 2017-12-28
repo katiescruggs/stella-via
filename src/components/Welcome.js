f687382cfa36a8a790f9d0bd1f466d990902bdb7
julie hawkins <julabi@gmail.com>
2017-12-28 10:26:27 -0700

    adds onpress to touchable highlights and labels for icons

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,25 +2,43 @@ import React, { Component } from 'react';
 import { StyleSheet, AppRegistry, TextInput, View, Text, Button, Image, TouchableHighlight } from 'react-native';
 
 const Welcome = (props) => {
+  handlePress = () => {
+    console.log('pressed')
+  } 
+
   return (
     <View style={styles.container}>
       <Text style={styles.mainTitle}>Stella Via</Text>
       <View style={styles.nav}>
-        <TouchableHighlight style={styles.navIcon}>
-          <Image source={require('../assets/hamburger.png')}/>
+
+        <TouchableHighlight style={styles.navIcon} onPress={handlePress}>
+          <View style={styles.iconWrapper}> 
+            <Text style={styles.navText}>Search</Text>
+            <Image source={require('../assets/hamburger.png')}/>
+          </View>
         </TouchableHighlight>
 
-        <TouchableHighlight style={styles.navIcon}>
-          <Image source={require('../assets/hamburger.png')}/>
+        <TouchableHighlight style={styles.navIcon} onPress={handlePress}>
+          <View style={styles.iconWrapper}> 
+            <Text style={styles.navText}>Tonight's Sky</Text>
+            <Image source={require('../assets/hamburger.png')}/>
+          </View>
         </TouchableHighlight>
 
-        <TouchableHighlight style={styles.navIcon}>
-          <Image source={require('../assets/hamburger.png')}/>
+        <TouchableHighlight style={styles.navIcon} onPress={handlePress}>
+          <View style={styles.iconWrapper}> 
+            <Text style={styles.navText}>Image of the Day</Text>
+            <Image source={require('../assets/hamburger.png')}/>
+          </View>
         </TouchableHighlight>
 
-        <TouchableHighlight style={styles.navIcon}>
-          <Image source={require('../assets/hamburger.png')}/>
+        <TouchableHighlight style={styles.navIcon} onPress={handlePress}>
+          <View style={styles.iconWrapper}> 
+            <Text style={styles.navText}>Login</Text>
+            <Image source={require('../assets/hamburger.png')}/>
+          </View>
         </TouchableHighlight>
+
       </View>
     </View>
   )
@@ -49,6 +67,10 @@ const styles = {
     padding: 20,
     margin: 20
   },
+  iconWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   navIcon: {
     height: 100, 
     width: 100,
@@ -56,7 +78,11 @@ const styles = {
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 20
+    margin: 20,
+  },
+  navText: {
+    textAlign: 'center'
+
   }
 }
 
