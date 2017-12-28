52213212af471b6daf04d00351c1b33e33d10e6c
julie hawkins <julabi@gmail.com>
2017-12-28 12:33:26 -0700

    adds icons and styles navIcons

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,34 +8,51 @@ const Welcome = (props) => {
 
   return (
     <View style={styles.container}>
+      {/*<Image source={require('../assets/stars-background.jpg')}/>*/}
       <Text style={styles.mainTitle}>Stella Via</Text>
       <View style={styles.nav}>
 
-        <TouchableHighlight style={styles.navIcon} onPress={handlePress}>
+        <TouchableHighlight 
+          style={styles.navIcon} 
+          onPress={handlePress} 
+          activeOpacity={0.3} 
+          underlayColor={'#735290'}>
           <View style={styles.iconWrapper}> 
             <Text style={styles.navText}>Search</Text>
-            <Image source={require('../assets/hamburger.png')}/>
+            <Image source={require('../assets/search.png')}/>
           </View>
         </TouchableHighlight>
 
-        <TouchableHighlight style={styles.navIcon} onPress={handlePress}>
+        <TouchableHighlight 
+          style={styles.navIcon} 
+          onPress={handlePress} 
+          activeOpacity={0.3} 
+          underlayColor={'#735290'}>
           <View style={styles.iconWrapper}> 
             <Text style={styles.navText}>Tonight's Sky</Text>
-            <Image source={require('../assets/hamburger.png')}/>
+            <Image source={require('../assets/planet.png')}/>
           </View>
         </TouchableHighlight>
 
-        <TouchableHighlight style={styles.navIcon} onPress={handlePress}>
+        <TouchableHighlight 
+          style={styles.navIcon} 
+          onPress={handlePress} 
+          activeOpacity={0.3} 
+          underlayColor={'#735290'}>
           <View style={styles.iconWrapper}> 
-            <Text style={styles.navText}>Image of the Day</Text>
-            <Image source={require('../assets/hamburger.png')}/>
+            <Text style={styles.navText}>Daily Image</Text>
+            <Image source={require('../assets/photo.png')}/>
           </View>
         </TouchableHighlight>
 
-        <TouchableHighlight style={styles.navIcon} onPress={handlePress}>
+        <TouchableHighlight 
+          style={styles.navIcon} 
+          onPress={handlePress} 
+          activeOpacity={0.3} 
+          underlayColor={'#735290'}>
           <View style={styles.iconWrapper}> 
             <Text style={styles.navText}>Login</Text>
-            <Image source={require('../assets/hamburger.png')}/>
+            <Image source={require('../assets/user.png')}/>
           </View>
         </TouchableHighlight>
 
@@ -47,42 +64,51 @@ const Welcome = (props) => {
 const styles = {
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center'
   },
   mainTitle: {
     fontSize: 90,
     color: '#fff',
-    margin: 20,
+    marginTop: 70,
+    margin: 15,
     textAlign: 'center'
   },
   nav: {
     borderRadius: 50,
     flexDirection: 'row',
     flexWrap: 'wrap',
-    borderWidth: 5,
+    borderWidth: 3,
     borderColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
-    margin: 20
+    paddingBottom: 25,
+    paddingTop: 30,
+    margin: 15,
   },
   iconWrapper: {
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    height: 130, 
+    width: 130,
   },
   navIcon: {
-    height: 100, 
-    width: 100,
-    backgroundColor: 'grey',
     borderRadius: 20,
+    backgroundColor: '#282640',
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 20,
+    margin: 10,
+    shadowColor: 'rgba(0, 0, 0, 0.8)',
+    shadowOffset: { width: 2, height: 10 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1
   },
   navText: {
-    textAlign: 'center'
-
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold'
   }
 }
 
