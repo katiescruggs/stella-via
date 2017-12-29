5c318d868bbbf546f68b60196fd0bf594ae4ab9b
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-28 19:52:33 -0700

    remove MainHeader and route directly to LocationModal and Constellations

--- src/containers/CurrentView.js
+++ src/containers/CurrentView.js
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 import Welcome from '../components/Welcome.js';
 import MainHeader from './MainHeader.js';
 import APOD from '../components/APOD.js';
+import LocationModal from './LocationModal.js';
+import Constellations from './Constellations.js';
 
 const CurrentView = ({page}) => {
   const pages = {
     welcome: <Welcome />,
-    LocationModal: <MainHeader />,
-    Constellations: <MainHeader />,
+    LocationModal: <LocationModal />,
+    Constellations: <Constellations />,
     APOD: <APOD />
   };
 
