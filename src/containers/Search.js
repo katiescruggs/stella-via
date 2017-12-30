3cdc5da513050e90863e47305b4e9797c8473b00
julie hawkins <julabi@gmail.com>
2017-12-29 17:49:42 -0700

    imports sky-map as WebView

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import NavBar from './NavBar.js';
+import { WebView } from 'react-native';
+
 
 const Search = () => (
   <View style={{height: '100%', paddingTop: 100}}>
     <Text>I am a search component! :)</Text>
+    <WebView
+        source={{uri: 'http://server1.sky-map.org/skywindow?ra=8&dec=45&show_constellation_lines=1&zoom=10&img_source=SDSS'}}
+        style={{width: '100%'}}
+      />
     <NavBar />
   </View>
 );
