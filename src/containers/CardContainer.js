6461df01d785baefba611fe844d71a99251b42a8
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-02 19:55:04 -0700

    take filter logic out of cardcontainer and put it in tonightssky

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -2,21 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { View } from 'react-native';
 import Card from '../components/Card.js'; 
-import constellations from '../../constellations/constellations.js';
 
-const CardContainer = ({RA, dec}) => {
-  const rangeRA = [RA - 4, RA + 4];
-  const rangeDec = [dec - 25, dec + 25];
-
-  const matchConstellations = constellations.filter(constellation => {
-    const { ra, dec } = constellation.coords;
-    const matchRA = ra > rangeRA[0] && ra < rangeRA[1];
-    const matchDec = dec > rangeDec[0] && dec < rangeDec[1];
-
-    return matchRA && matchDec;
-  });
-
-  const cards = matchConstellations.map((constellation, index) => (
+const CardContainer = ({RA, dec, constellations}) => {
+  const cards = constellations.map((constellation, index) => (
     <Card
       key={`card-${index}`} 
       constellation={constellation}
--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,19 +1,68 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
 import NavBar from './NavBar.js';
+import { colors } from '../assets/colors.js';
 
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
 
-const Search = () => (
-  <View style={styles.container}>
-    <Text>I am a search component! :)</Text>
-    <NavBar />
-  </View>
-);
+  handleSearch = () => {
+    console.log(this.state);
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.searchTitle}>Search</Text>
+        <TextInput
+          style = {styles.textInput}
+          value={this.state.text}
+          placeholder='Search for a constellation.'
+          onChangeText={(text) => this.setState({text})}
+        />
+        <TouchableHighlight 
+          style={styles.button}
+          onPress={this.handleSearch}>
+          <Text style={styles.buttonText}>Search</Text>
+        </TouchableHighlight>
+        <NavBar />
+      </View>
+    );
+  }
+} 
 
 const styles = StyleSheet.create({
+  searchTitle: {
+    alignSelf: 'center',
+    fontSize: 30,
+    marginBottom: 20
+  },
   container: {
     height: '100%', 
     paddingTop: 100
+  },
+  textInput: {
+    borderColor: colors.$redPurple,
+    borderWidth: 1,
+    fontSize: 20,
+    padding: 10,
+    marginBottom: 10
+  },
+  button: {
+    alignSelf: 'center',
+    backgroundColor: colors.$lavender,
+    borderColor: colors.$lavender,
+    borderRadius: 25,
+    borderWidth: 2,
+    padding: 10
+  },
+  buttonText: {
+    fontSize: 20
   }
 });
 
