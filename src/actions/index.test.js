dcc85fb78d4f429752d28cc598f32c11ef23a6a0
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-03 19:53:01 -0700

    passing tests for all 5 actions

---
+++ src/actions/index.test.js
@@ -0,0 +1,58 @@
+import * as actions from './index';
+
+describe('all actions', () => {
+  it('setLocation action', () => {
+    const location = {
+      lat: 50,
+      lon: 50
+    };
+
+    const expected = {
+      type: 'SET_LOCATION',
+      location
+    };
+
+    expect(actions.setLocation(location)).toEqual(expected);
+  });
+
+  it('changePage action', () => {
+    const page = 'next';
+
+    const expected = {
+      type: 'CHANGE_PAGE',
+      page
+    };
+
+    expect(actions.changePage(page)).toEqual(expected);
+  });
+
+  it('setSkyCoords action', () => {
+    const skyCoords = {
+      dec: 50,
+      ra: 1000
+    };
+
+    const expected = {
+      type: 'SET_SKY_COORDS',
+      skyCoords
+    };
+
+    expect(actions.setSkyCoords(skyCoords)).toEqual(expected);
+  });
+
+  it('setAPOD action', () => {
+    const apodData = {
+      image: 'im an image',
+      type: 'image',
+      title: 'TITLE',
+      details: 'I am an explanation'
+    };
+
+    const expected = {
+      type: 'SET_APOD',
+      apodData
+    };
+
+    expect(actions.setAPOD(apodData)).toEqual(expected);
+  });
+});
