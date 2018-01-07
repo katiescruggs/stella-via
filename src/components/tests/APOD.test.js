251ac54d85483b9749c1ecf639756f76d532b98a
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-06 18:39:44 -0700

    mapStateToProps test for APOD

---
+++ src/components/tests/APOD.test.js
@@ -0,0 +1,23 @@
+import { APOD, mapStateToProps } from '../APOD';
+
+describe('APOD container', () => {
+  describe('APOD component', () => {
+
+  });
+
+  describe('mapStateToProps', () => {
+    it('should pull apodData from the store', () => {
+      const mockStore = {
+        apodData: {
+          image: 'url',
+          type: 'image',
+          title: 'APOD',
+          details: 'explanation'
+        }
+      };
+
+        const result = mapStateToProps(mockStore);
+        expect(result.apodData).toEqual(mockStore.apodData);
+    });
+  });
+});
