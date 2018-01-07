d227568304e338177f0e2d02dd23fe0fc1c1f0f5
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-06 19:08:56 -0700

    CurrentView tests complete

---
+++ src/containers/tests/CurrentView.test.js
@@ -0,0 +1,20 @@
+import { CurrentView, mapStateToProps } from '../CurrentView';
+
+describe('CurrentView Container', () => {
+  describe('CurrentView component', () => {
+    it('should exist', () => {
+      expect(CurrentView).toBeDefined();
+    });
+  });
+
+  describe('mapStateToProps', () => {
+    it('should pull current page from the store', () => {
+      const mockStore = {
+        page: 'Welcome'
+      };
+
+      const result = mapStateToProps(mockStore)
+      expect(result.page).toEqual(mockStore.page);
+    });
+  });
+});
