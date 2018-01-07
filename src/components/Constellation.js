e6417afed183f11ef2aa624f66066b9c046a926d
julie hawkins <julabi@gmail.com>
2018-01-06 17:46:32 -0700

    adds headers to fullview constellation

--- src/components/Constellation.js
+++ src/components/Constellation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { StyleSheet, View, Text, Image, ImageBackground, TouchableHighlight } from 'react-native';
+import { StyleSheet, View, Text, Image, ImageBackground, TouchableHighlight, ScrollView } from 'react-native';
 import { colors } from '../assets/colors';
 import { changePage } from '../actions';
 import NavBar from '../containers/NavBar';
@@ -39,44 +39,53 @@ const Constellation = ({ constellation, currentPage, changePage }) => {
     <ImageBackground
       source={require('../assets/star-background.jpg')}
       style={styles.container}>
-        <View style={styles.header}>
-          {visibleEye}
-          <Text style={styles.title}>
-            {name.toUpperCase()}
-          </Text>
-        </View>
-        <View style={styles.imageContainer}>
-          <TouchableHighlight 
-            style={styles.button}
-            onPress={handlePress} 
-            activeOpacity={0.3} 
-            underlayColor={colors.$darkPurple}>
-            <Image 
-              style={styles.icon}
-              source={require('../assets/icons/go-back.png')}/>
-          </TouchableHighlight>
+      <View style={styles.header}>
+        {visibleEye}
+        <Text style={styles.title}>
+          {name.toUpperCase()}
+        </Text>
+        <Text style={styles.translation}>
+          {`"${translation}"`}
+        </Text>
+      </View>
+      <View style={styles.imageContainer}>
+        <TouchableHighlight 
+          style={styles.button}
+          onPress={handlePress} 
+          activeOpacity={0.3} 
+          underlayColor={colors.$darkPurple}>
           <Image 
-            source={image}
-            style={styles.constellationImage}
-          />
-          <Text style={styles.translation}>
-            {`"${translation}"`}
-          </Text>
-        </View>
+            style={styles.icon}
+            source={require('../assets/icons/go-back.png')}/>
+        </TouchableHighlight>
+        <Image 
+          source={image}
+          style={styles.constellationImage}
+        />
+      </View>
+      <ScrollView style={{width: '100%'}}>
         <View style={styles.detailsContainer}>
-          <Text style={styles.detailText}>
-          {`Location: RA ${coords.ra}, DEC ${coords.dec}`}
-          </Text>
-          <View style={styles.stars}> 
-            <Text style={styles.detailText}>
-              {`Named Stars: ${starsString}`}
-            </Text>
+          <View style={styles.detailHeader}>          
+            <Text style={styles.detailHeaderText}>Location:</Text>
           </View>
           <Text style={styles.detailText}>
-            {`Description: ${description}`}
+          {`RA ${coords.ra}, DEC ${coords.dec}`}
+          </Text>
+          <View style={styles.detailHeader}>          
+            <Text style={styles.detailHeaderText}>Named Stars:</Text>
+          </View>
+          <Text style={styles.detailText}>
+            {`${starsString}`}
+          </Text>
+          <View style={styles.detailHeader}>          
+            <Text style={styles.detailHeaderText}>Description:</Text>
+          </View>
+          <Text style={styles.detailText}>
+            {`${description}`}
           </Text>
         </View>
-        <NavBar />
+      </ScrollView>
+      <NavBar />
     </ImageBackground>
   )
 }
@@ -86,7 +95,6 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     alignItems: 'center', 
-    justifyContent: 'space-between'
   },
   header: {
     backgroundColor: colors.$purple,
@@ -107,6 +115,16 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  translation: {
+    backgroundColor: colors.$purple,
+    color: colors.$white,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+    width: '100%'
   },
   imageContainer: {
     alignItems: 'center',
@@ -130,24 +148,30 @@ const styles = StyleSheet.create({
     height: 40,
     width: 40
   },
-  translation: {
-    backgroundColor: colors.$purple,
-    color: colors.$white,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    paddingTop: 10,
-    paddingBottom: 10,
-    width: '100%'
-  },
   detailsContainer: {
     marginBottom: 120,
+  },
+  detailHeader: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.$lavender,
+    width: '100%'
+  },
+  detailHeaderText: {
+    backgroundColor: colors.$purple,
+    color: colors.$white,
+    padding: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    margin: 5,
+    width: '100%'
   },
   detailText: {
     backgroundColor: 'transparent',
     color: colors.$white,
     padding: 5,
-    fontSize: 18,
+    fontSize: 16,
+    margin: 5
   }
 });
 
