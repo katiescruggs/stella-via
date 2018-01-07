1eeacc360c18b4550e838f76b538f157e7e557ba
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-07 15:00:22 -0700

    update StarMap tests to reflect that StarMap no longer pulls in lat and lon from the store

--- src/components/tests/StarMap.test.js
+++ src/components/tests/StarMap.test.js
@@ -9,10 +9,6 @@ describe('StarMap Container', () => {
 
   describe('mapStateToProps', () => {
     const mockStore = {
-      location: {
-        lat: 39,
-        lon: -105
-      },
       skyCoords: {
         dec: 39,
         RA: '4 30 0'
@@ -21,13 +17,11 @@ describe('StarMap Container', () => {
 
     const result = mapStateToProps(mockStore);
     
-    it('should pull lat and lon from the location in store', () => {
-      expect(result.lat).toEqual(mockStore.location.lat);
-      expect(result.lon).toEqual(mockStore.location.lon);
+    it('should pull dec from the skyCoords in store', () => {
+      expect(result.dec).toEqual(mockStore.skyCoords.dec);
     });
 
-    it('should pull dec and RA from the skyCoords in store', () => {
-      expect(result.dec).toEqual(mockStore.skyCoords.dec);
+    it('should pull RA from the skyCoords in store', () => {
       expect(result.RA).toEqual(mockStore.skyCoords.stringRA);
     });
   });
