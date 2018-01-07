11353809b3035c35e74c0b4af55af0af1aa4d0a4
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-06 18:57:24 -0700

    touch Welcome test file and import functions

---
+++ src/components/tests/Welcome.test.js
@@ -0,0 +1,15 @@
+import { Welcome, mapStateToProps, mapDispatchToProps } from '../Welcome';
+
+describe('Welcome Container', () => {
+  describe('Welcome Component', () => {
+
+  });
+
+  describe('mapStateToProps', () => {
+
+  });
+
+  describe('mapDispatchToProps', () => {
+
+  });
+});
