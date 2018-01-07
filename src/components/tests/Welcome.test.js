b1483b702cda5c2fe0cc2648c8695a9d3ddf59e2
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-06 18:58:42 -0700

    mapStateToProps, mapDispatchToProps tests for Welcome

--- src/components/tests/Welcome.test.js
+++ src/components/tests/Welcome.test.js
@@ -6,10 +6,31 @@ describe('Welcome Container', () => {
   });
 
   describe('mapStateToProps', () => {
+    it('should pull location from the store', () => {
+      const mockStore = {
+        location: 'Welcome'
+      };
 
+      const result = mapStateToProps(mockStore)
+      expect(result.location).toEqual(mockStore.location);
+    });
   });
 
   describe('mapDispatchToProps', () => {
+    it('should call dispatch when changePage is called', () => {
+      const mockDispatch = jest.fn();
+      const result = mapDispatchToProps(mockDispatch);
 
+      result.changePage();
+      expect(mockDispatch).toHaveBeenCalled;
+    });
+
+    it('should call dispatch when setAPOD is called', () => {
+      const mockDispatch = jest.fn();
+      const result = mapDispatchToProps(mockDispatch);
+
+      result.setAPOD();
+      expect(mockDispatch).toHaveBeenCalled;
+    });
   });
 });
