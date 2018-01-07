56d409763fd7ddc6bc1b05169fa2329c30b7eaeb
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-06 18:50:43 -0700

    test mapStateToProps and mapDispatchToProps in Constellation

--- src/components/tests/Constellation.test.js
+++ src/components/tests/Constellation.test.js
@@ -5,5 +5,33 @@ describe('Constellation Container', () => {
 
   });
 
+  describe('mapStateToProps', () => {
+    it('should pull currentPage from the store', () => {
+      const mockStore = {
+        page: 'Welcome'
+      };
 
+      const result = mapStateToProps(mockStore)
+      expect(result.currentPage).toEqual(mockStore.page);
+    });
+
+    it('should pull constellation from the store', () => {
+      const mockStore = {
+        constellation: 'Pisces'
+      };
+
+      const result = mapStateToProps(mockStore);
+      expect(result.constellation).toEqual(mockStore.constellation);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch when changePage is called', () => {
+      const mockDispatch = jest.fn();
+      const result = mapDispatchToProps(mockDispatch);
+
+      result.changePage();
+      expect(mockDispatch).toHaveBeenCalled;
+    });
+  });
 });
