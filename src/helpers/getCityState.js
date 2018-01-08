82936b458d81d51d4246d661b93670569a7716f1
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-07 19:48:08 -0700

    make eslint happy

--- src/helpers/getCityState.js
+++ src/helpers/getCityState.js
@@ -4,5 +4,6 @@ export const getCityState = async (lat, lon) => {
   const initialFetch = await fetch(`https://api.wunderground.com/api/${wundergroundKey}/geolookup/q/${lat},${lon}.json`);
   const { location } = await initialFetch.json();
   const { city, state } = location;
+  
   return { city, state };
 };
