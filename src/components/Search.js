7a107969ef30db8ef6b758e39539c53b301eca37
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-07 18:12:38 -0700

    fix bottom padding on Explore page due to margin changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -127,7 +127,7 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
-    paddingBottom: 100
+    paddingBottom: 80
   },
   textInput: {
     backgroundColor: colors.$white,
