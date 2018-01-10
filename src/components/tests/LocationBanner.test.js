1d56b08c460188e7de924fd419895c4cfcc05c4d
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-10 15:50:22 -0700

    update tests with logic changes to starmap

--- src/components/tests/LocationBanner.test.js
+++ src/components/tests/LocationBanner.test.js
@@ -36,14 +36,6 @@ describe('LocationBanner Container', () => {
       expect(result.location.city).toEqual(mockStore.location.city);
       expect(result.location.state).toEqual(mockStore.location.state);
     });
-
-    it('should pull in dec from the store\'s skyCoords', () => {
-      expect(result.dec).toEqual(mockStore.skyCoords.dec);
-    });
-
-    it('should pull in RA from the stringRA in the store\'s skyCoords', () => {
-      expect(result.RA).toEqual(mockStore.skyCoords.stringRA);
-    });
   });
 
   describe('mapDispatchToProps', () => {
